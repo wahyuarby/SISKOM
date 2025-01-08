e6097816070b533a7ed19419550de195d224fa5d
wahyuarby <wahyuarbymuhammad@gmail.com>
2025-01-08 17:47:25 +0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import seaborn as sns
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
